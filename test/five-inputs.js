85efc10b01b01d8f875e593ef84847f469d8da8d
fivdi <fivdiweb@gmail.com>
2013-05-26 10:07:13 +0200

    added test with five inputs

---
+++ test/five-inputs.js
@@ -0,0 +1,33 @@
+var Gpio = require('../onoff').Gpio,
+    pins = [18, 22, 23, 24, 27],
+    buttons = [],
+    presses = 0,
+    i = 0;
+
+function buttonPressed(err, value) {
+    var j = 0;
+
+    presses += 1;
+
+    console.log('Button on GPIO #' + this.gpio + ' pressed. ' + presses + ' button presses so far.');
+
+    if (presses === 10) {
+        for (j = 0; j !== buttons.length; j += 1) {
+            buttons[j].unexport();
+        }
+
+        process.kill(process.pid, 'SIGHUP');
+    }
+}
+
+for (i = 0; i !== pins.length; i += 1) {
+    buttons[i] = new Gpio(pins[i], 'in', 'rising', {
+        debounceTimeout : 250,
+        persistentWatch : true
+    });
+
+    buttons[i].watch(buttonPressed.bind(buttons[i]));
+}
+
+console.info('Please press buttons attached to GPIO #18, #22, #23, #24, or #27...');
+
