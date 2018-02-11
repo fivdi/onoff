05e41079baaf81eb697d1a096a928c5f61a4fdc2
fivdi <bdscooke@gmail.com>
2018-02-11 13:06:23 +0100

    add failing test for #87

---
+++ test/output-with-edge-bug.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// Test for https://github.com/fivdi/onoff/issues/87
+//
+// If a Gpio is instantiated for an output GPIO and the edge parameter is
+// specified then the edge parameter should be ignored. Attempting to write
+// the sysfs edge file for an output GPIO results in an
+// "EIO: i/o error, write"
+
+
+var Gpio = require('../onoff').Gpio,
+  assert = require('assert');
+
+function ensureGpio17Unexported(cb) {
+  var led = new Gpio(17, 'out');
+
+  led.unexport();
+
+  setTimeout(function () {
+    cb();
+  }, 100);
+}
+
+ensureGpio17Unexported(function () {
+  assert.doesNotThrow(
+    () => {new Gpio(17, 'out', 'both');},
+    'can\'t instantiate a Gpio for an output with edge option specified'
+  );
+
+  console.log('ok - ' + __filename);
+});
+
