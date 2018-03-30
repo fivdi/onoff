c1389286e7cfae7d91a487102fd55815b88e7eb1
fivdi <bdscooke@gmail.com>
2018-03-30 22:04:03 +0200

    remove unmaintained comments

--- onoff.js
+++ onoff.js
@@ -8,36 +8,6 @@ var GPIO_ROOT_PATH = '/sys/class/gpio/',
   ZERO = new Buffer('0'),
   ONE = new Buffer('1');
 
-/**
- * Constructor. Exports a GPIO to userspace.
- *
- * The constructor is written to function for both superusers and
- * non-superusers. See README.md for more details.
- *
- * gpio: number      // The Linux GPIO identifier; an unsigned integer.
- * direction: string // Specifies whether the GPIO should be configured as an
- *                   // input or output. The valid values are: 'in', 'out',
- *                   // 'high', and 'low'. 'high' and 'low' are variants of
- *                   // 'out' that configure the GPIO as an output with an
- *                   // initial level of high or low respectively.
- * [edge: string]    // The interrupt generating edge for the GPIO. Can be
- *                   // specified for GPIO inputs and outputs. The edge
- *                   // specified determine what watchers watch for. The valid
- *                   // values are: 'none', 'rising', 'falling' or 'both'.
- *                   // The default value is 'none'. [optional]
- * [options: object] // Additional options. [optional]
- *
- * The options argument supports the following:
- * debounceTimeout: number  // Can be used to software debounce a button or
- *                          // switch using a timeout. Specified in
- *                          // milliseconds. The default value is 0.
- * activeLow: boolean       // Specifies whether the values read from or
- *                          // written to the GPIO should be inverted. The
- *                          // interrupt generating edge for the GPIO also
- *                          // follow this this setting. The valid values for
- *                          // activeLow are true and false. Setting activeLow
- *                          // to true inverts. The default value is false.
- */
 function Gpio(gpio, direction, edge, options) {
   var permissionRequiredPaths;
 
@@ -171,11 +141,6 @@ function Gpio(gpio, direction, edge, options) {
 }
 exports.Gpio = Gpio;
 
-/**
- * Read GPIO value asynchronously.
- *
- * [callback: (err: error, value: number) => {}] // Optional callback
- */
 Gpio.prototype.read = function (callback) {
   fs.read(this.valueFd, this.readBuffer, 0, 1, 0, function (err, bytes, buf) {
     if (typeof callback === 'function') {
@@ -188,54 +153,21 @@ Gpio.prototype.read = function (callback) {
   });
 };
 
-/**
- * Read GPIO value synchronously.
- *
- * Returns - number // 0 or 1
- */
 Gpio.prototype.readSync = function () {
   fs.readSync(this.valueFd, this.readBuffer, 0, 1, 0);
   return this.readBuffer[0] === ONE[0] ? 1 : 0;
 };
 
-/**
- * Write GPIO value asynchronously.
- *
- * value: number                  // 0 or 1
- * [callback: (err: error) => {}] // Optional callback
- */
 Gpio.prototype.write = function (value, callback) {
   var writeBuffer = value === 1 ? ONE : ZERO;
   fs.write(this.valueFd, writeBuffer, 0, writeBuffer.length, 0, callback);
 };
 
-/**
- * Write GPIO value synchronously.
- *
- * value: number // 0 or 1
- */
 Gpio.prototype.writeSync = function (value) {
   var writeBuffer = value === 1 ? ONE : ZERO;
   fs.writeSync(this.valueFd, writeBuffer, 0, writeBuffer.length, 0);
 };
 
-/**
- * Watch for hardware interrupts on the GPIO. Inputs and outputs can be
- * watched. The edge argument that was passed to the constructor determines
- * which hardware interrupts are watcher for. The 3.13 kernel dropped support
- * for interrupt generating outputs, irrespective of whether the underlying
- * hardware supports them or not.
- *
- * Note that the value passed to the callback does not represent the value of
- * the GPIO the instant the interrupt occured, it represents the value of the
- * GPIO the instant the GPIO value file is read which may be several
- * milliseconds after the actual interrupt. By the time the GPIO value is read
- * the value may have changed. There are scenarios where this is likely to
- * occur, for example, with buttons or switches that are not hadrware
- * debounced.
- *
- * callback: (err: error, value: number) => {}
- */
 Gpio.prototype.watch = function (callback) {
   this.listeners.push(callback);
 
@@ -244,9 +176,6 @@ Gpio.prototype.watch = function (callback) {
   }
 };
 
-/**
- * Stop watching for hardware interrupts on the GPIO.
- */
 Gpio.prototype.unwatch = function (callback) {
   if (this.listeners.length > 0) {
     if (typeof callback !== 'function') {
@@ -263,93 +192,39 @@ Gpio.prototype.unwatch = function (callback) {
   }
 };
 
-/**
- * Remove all watchers for the GPIO.
- */
 Gpio.prototype.unwatchAll = function () {
   this.unwatch();
 };
 
-/**
- * Get GPIO direction.
- *
- * Returns - string // 'in', or 'out'
- */
 Gpio.prototype.direction = function () {
   return fs.readFileSync(this.gpioPath + 'direction').toString().trim();
 };
 
-/**
- * Set GPIO direction.
- *
- * direction: string // Specifies whether the GPIO should be configured as an
- *                   // input or output. The valid values are: 'in', 'out',
- *                   // 'high', and 'low'. 'high' and 'low' are variants of
- *                   // 'out' that configure the GPIO as an output with an
- *                   // initial level of high or low respectively.
- */
 Gpio.prototype.setDirection = function (direction) {
   fs.writeFileSync(this.gpioPath + 'direction', direction);
 };
 
-/**
- * Get GPIO interrupt generating edge.
- *
- * Returns - string // 'none', 'rising', 'falling' or 'both'
- */
 Gpio.prototype.edge = function () {
   return fs.readFileSync(this.gpioPath + 'edge').toString().trim();
 };
 
-/**
- * Set GPIO interrupt generating edge.
- *
- * edge: string // The interrupt generating edge for the GPIO. Can be
- *              // specified for GPIO inputs and outputs. The edge
- *              // specified determine what watchers watch for. The valid
- *              // values are: 'none', 'rising', 'falling' or 'both'.
- */
 Gpio.prototype.setEdge = function (edge) {
   fs.writeFileSync(this.gpioPath + 'edge', edge);
 };
 
-/**
- * Get GPIO activeLow setting.
- *
- * Returns - boolean
- */
 Gpio.prototype.activeLow = function () {
   return fs.readFileSync(
     this.gpioPath + 'active_low')[0] === ONE[0] ? true : false;
 };
 
-/**
- * Set GPIO activeLow setting.
- *
- * invert: boolean // Specifies whether the values read from or
- *                 // written to the GPIO should be inverted. The
- *                 // interrupt generating edge for the GPIO also
- *                 // follow this this setting. The valid values for
- *                 // activeLow are true and false. Setting activeLow
- *                 // to true inverts. The default value is false.
- */
 Gpio.prototype.setActiveLow = function (invert) {
   fs.writeFileSync(this.gpioPath + 'active_low', !!invert ? ONE : ZERO);
 };
 
-/**
- * Get GPIO options.
- *
- * Returns - object // Must not be modified
- */
 Gpio.prototype.options = function () {
   return this.opts;
 };
 
-/**
- * Reverse the effect of exporting the GPIO to userspace. The Gpio object
- * should not be used after calling this method.
- */
 Gpio.prototype.unexport = function () {
   this.unwatchAll();
   fs.closeSync(this.valueFd);
