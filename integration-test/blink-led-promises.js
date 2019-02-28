90ef304726ea6eb4de550ba72546a4059288b4be
fivdi <bdscooke@gmail.com>
2019-02-28 13:32:35 +0100

    add integration test for promises

---
+++ integration-test/blink-led-promises.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const Gpio = require('../onoff').Gpio;
+const led = new Gpio(17, 'out');
+let stopBlinking = false;
+
+const blinkLed = () => {
+  if (stopBlinking) {
+    led.unexport();
+    console.log('ok - ' + __filename);
+    return;
+  }
+
+  led.read()
+    .then(value => led.write(value ^ 1))
+    .then(_ => setTimeout(blinkLed, 50))
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+blinkLed();
+
+setTimeout(_ => stopBlinking = true, 2000);
+
+
