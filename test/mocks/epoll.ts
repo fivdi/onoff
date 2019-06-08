6f1c9ecf67a2ee0b965694b682c557268171655a
pizzaisdavid <davidispizza@gmail.com>
2019-06-08 02:12:41 +0200

    make timeout into a private variable

--- test/mocks/epoll.ts
+++ test/mocks/epoll.ts
@@ -4,22 +4,22 @@ export class Epoll {
   static get EPOLLPRI(): number { return 2; }
 
   _callback: any;
-  _timeout: any;
+  private timeout: any;
 
   constructor(callback: (error: Error | null | undefined, fd: any, events: any) => void) {
     this._callback = callback;
-    this._timeout = null;
+    this.timeout = null;
   }
 
   add(fd: any, events: any) {
-    this._timeout = setTimeout(() => {
+    this.timeout = setTimeout(() => {
       this._callback(null, fd, events);
     }, 10);
   }
 
   remove(fd: any) {
-    if (this._timeout !== null) {
-      clearTimeout(this._timeout);
+    if (this.timeout !== null) {
+      clearTimeout(this.timeout);
     }
   }
 }
